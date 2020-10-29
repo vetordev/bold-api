ec8fb68ba6e6e64ea2be2a670c4a057502a993ad
vetordev <vitor13m2003@gmail.com>
2020-10-29 20:52:45 -0300

    test: Testes unitários

--- src/tests/unit/emailExists.spec.ts
+++ src/tests/unit/emailExists.spec.ts
@@ -6,24 +6,33 @@ import Database from '../../database';
 
 describe('Verificação do e-mail do usuário', () => {
   beforeAll(async () => {
-    Database.connect();
-
-    await User.create({
+    const user = {
       nome: 'Vitor',
-      email: 'vitor.14@gmail.com',
+      email: 'vitor.13@gmail.com',
       senha: hashPassword('seNhA4'),
       telefones: [{
         numero: '952316039',
         ddd: '11',
       }],
       ultimo_login: new Date(),
-      token: generateTokenJwt('vitor.14@gmail.com'),
-    });
+      token: generateTokenJwt('vitor.13@gmail.com'),
+    };
+
+    Database.connect();
+
+    await User.findOneAndDelete({ email: user.email });
+
+    await User.create(user);
   });
 
   afterAll(() => {
     Database.disconnect();
   });
 
-  it('', () => {});
+  it('Deve encontrar o e-mail do usuário', async () => {
+    const user: any = await emailExists('vitor.13@gmail.com');
+
+    expect(user).not.toBeNull();
+    expect(user.email).toBe('vitor.13@gmail.com');
+  });
 });
